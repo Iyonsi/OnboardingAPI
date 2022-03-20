ee9482e2ea50bba6fa117d484957137e286ebbf0
Israel Iyonsi <israel.iyonsi@decagon.dev>
2022-03-20 22:26:12 +0100

    Add project files.

---
+++ OtpAPI/OtpAPI.Controllers/OtpController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OtpApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class OtpController : ControllerBase
+    {
+        
+    }
+}
